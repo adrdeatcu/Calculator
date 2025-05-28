14be3f6b1c852f89d16026fda39a300ae6b7a3d7
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-05-29 00:42:42 +0300

    feat: operations functions + operate function for each case

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log(1);
+function add(a,b)
+{
+    return a+b;
+}
+
+function subtract(a,b)
+{
+    return a-b;
+}
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+function divide(a,b)
+{
+    return a/b
+}
+
+function procent(number)
+{
+    return number/100;
+}
+
+function operate(operator,a,b)
+{
+    if(operator=="+")
+    {
+        return add(a,b);
+    }else if(operator=="-")
+    {
+        return subtract(a,b);
+    }else if(operator=="*")
+    {
+        return multiply(a,b);
+    }else
+    {
+        return divide(a,b);
+    }
+}
+
+let finalResult = 0;
+console.log(operate("-",10,5));
+
+finalResult=operate("+",5,6);
+console.log(finalResult);
